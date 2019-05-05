627cb20bed7ed718dad9f614f71e11264d371a52
ahmederaqi <ahmed.eraqi@elite-1st.com>
2019-05-05 12:19:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// const inventors = [
+//     { first: "ahmed", last: "wael", year: 2021, passed: 2030 },
+//     { first: "mohamed", last: "islam", year: 1852, passed: 2035 },
+//     { first: "said", last: "hassan", year: 1944, passed: 2042 },
+//     { first: "islam", last: "ibramin", year: 2016, passed: 2045 },
+//     { first: "mahmoud", last: "saad", year: 1992, passed: 2050 },
+//     { first: "esraa", last: "mostafa", year: 1992, passed: 2060 },
+//     { first: "alaa", last: "mohamed", year: 2015, passed: 2065 },
+//     { first: "dina", last: "albert", year: 2018, passed: 2067 },
+//     { first: "bassant", last: "ismail", year: 2019, passed: 2025 },
+// ]
+
+// const people = [
+//     'ahmed', "islam", "mohamed", "noha", "samira", "bassant", "ismail", "wael", "hind", "abdo",
+//     "nada", "esso", "salem", "hema"
+// ]
+
+// const ff = inventors.filter(item => item.passed < 2045)
+
+// const fullName = inventors.map(item =>`my firstName is (${item.first}) and my lastName id ((${item.last}))`);
+
+// // console.table(ff)
+// console.table(fullName)
